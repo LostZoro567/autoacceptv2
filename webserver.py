141f92cb73e54705db79efb8bc371f16b346baa8
LostZoro567 <149526552+LostZoro567@users.noreply.github.com>
2025-09-08 21:12:50 +0530

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,16 +1,18 @@
 # webserver.py
 from aiohttp import web
 import os
+from config import logger
 
-async def handle(request):
-    return web.Response(text="Bot is running!")
+async def handle_root(request):
+    return web.Response(text="Bot running!")
 
 async def start_web_server():
     app = web.Application()
-    app.router.add_get("/", handle)
+    app.router.add_get("/", handle_root)
 
-    port = int(os.getenv("PORT", 8080))
+    port = int(os.getenv("PORT", os.getenv("RENDER_PORT", 8000)))
     runner = web.AppRunner(app)
     await runner.setup()
     site = web.TCPSite(runner, "0.0.0.0", port)
     await site.start()
+    logger.info(f"HTTP server started on port {port}")
